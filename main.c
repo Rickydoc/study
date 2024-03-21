860b71d7bb2367dd02cde27fd585f7cc7dacfb7e
Rickydoc <153089431+Rickydoc@users.noreply.github.com>
2024-03-21 15:38:24 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,197 @@
+#include "data.h"
+int main()
+{
+    int j, count = 0, k;
+    int x,charge=0;
+    long int hash=7,key;
+    char n, i = '1', t[20], m;
+
+    // 创建一个card型指针，用来在链表上移动，精确操作链表每个节点
+    card *member;
+
+    // 创建一个cardlist型结构，用来汇总链表信息，存储链表的头节点以及节点数量，方便找到每个节点
+    cardlist memberlist;
+
+    // 创建链表并初始化链表信息，此时创建链表第一个节点，并记录头节点，初始化节点数量
+    memberlist.head = addcard();
+    memberlist.count = 1;
+
+    // 让指针指向链表第一个节点，链表创建完成
+    member = memberlist.head;
+
+    // TODO读取文件
+    // 初始化链表信息，将上一次程序运行的数据读取到当前程序
+    FILE *p;
+    if ((p = fopen("member.txt", "r")) == NULL)
+    {
+        // 检测是否存在能够读取成员信息的文件
+        printf("无法查询到以前的成员信息，该文件不存在或者已经被损坏！\n");
+        printf("请重新录入以往成员信息！\n");
+        fclose(p);
+    }
+    else
+    {
+        //TODO检测保存数据的文件，判断文件是否被篡改
+        while((x=fgetc(p))!=EOF)
+	    {
+		    hash+=x+55;
+            hash+=hash/2;
+		    hash++;
+	    }
+        fclose(p);
+        if ((p = fopen("hash.txt", "r")) == NULL)
+	    {
+		    printf("Error：哈希文件丢失，无法判断成员信息是否被篡改！\n");
+            printf("请重新录入以往成员信息！\n");
+		    fclose(p);
+            charge=0;
+	    }
+        else
+        {
+            fscanf(p,"%ld",&key);
+            fclose(p);
+            if (hash==key)
+                charge=1;
+            else
+                charge=-1;
+        }
+
+        //哈希值不匹配，则不再读取数据并清除所有数据文件
+        if (charge!=1)
+        {
+            remove("member.txt");
+            if (charge==-1)
+            {
+                printf("Error：哈希值不匹配！数据文件被篡改！无法读取成员信息！\n");
+                printf("请重新录入以往成员信息！\n");
+            }
+        }
+        else
+        {
+            //数据一切正常，开始格式化读取信息
+            // 读取第一行的节点数量
+            p = fopen("member.txt", "r");
+            fscanf(p, " %d", &memberlist.count);
+            fgetc(p);
+            for (k = 1; k < memberlist.count; k++)
+            {
+                for (j = 0; j < 40; j++)
+                {
+                    member->id[j] = '\0';
+                    member->password[j] = '\0';
+                }
+                for (j = 0; 1; j++)
+                {
+                    m = fgetc(p);
+                    if (m != '\n')
+                        member->id[j] = m;
+                    else
+                    {
+                        member->id[j] = '\0';
+                        break;
+                    }
+                }
+                for (j = 0; 1; j++)
+                {
+                    m = fgetc(p);
+                    if (m != '\n')
+                        member->password[j] = m;
+                    else
+                    {
+                        member->password[j] = '\0';
+                        break;
+                    }
+                }
+                fscanf(p," %d",&member->status);
+                fscanf(p," %d",&member->starttime.year);
+                fscanf(p," %d",&member->starttime.month);
+                fscanf(p," %d",&member->starttime.day);
+                fscanf(p," %d",&member->starttime.hour);
+                fscanf(p," %d",&member->starttime.min);
+                fscanf(p," %d",&member->starttime.sec);
+                fscanf(p," %f",&member->totaluse);
+                fscanf(p," %d",&member->count);
+                fscanf(p," %f",&member->balance);
+                fscanf(p," %d",&member->del);
+                fscanf(p," %d",&member->lasttime.year);
+                fscanf(p," %d",&member->lasttime.month);
+                fscanf(p," %d",&member->lasttime.day);
+                fscanf(p," %d",&member->lasttime.hour);
+                fscanf(p," %d",&member->lasttime.min);
+                fscanf(p," %d",&member->lasttime.sec);
+                fscanf(p," %d",&member->endtime.year);
+                fscanf(p," %d",&member->endtime.month);
+                fscanf(p," %d",&member->endtime.day);
+                fscanf(p," %d",&member->endtime.hour);
+                fscanf(p," %d",&member->endtime.min);
+                fscanf(p," %d",&member->endtime.sec);
+                m = fgetc(p);
+                //TODO循环结束记得增加新的节点存储信息
+                member->next=addcard();
+                member=member->next;
+            }
+            fclose(p);
+        }
+    }
+    //读取文件操作完成，指针指向头结点，开始进入菜单界面
+    member=memberlist.head;
+    printf("欢迎来到计费管理系统面板\n");
+    do
+    {
+        for (j = 0; j < 20; j++)
+            t[j] = '\0';
+        outputmenu();
+        gets(t);
+        // 检测输入信息是否合法
+        if (t[1] != '\0')
+        {
+            printf("Error：无法识别输入信息，请重新操作！\n");
+            continue;
+        }
+        n = t[0];
+        // 信息合法则开始对应相应的功能
+        switch (n)
+        {
+        case '0':
+            i = func0();
+            break;
+        case '1':
+            memberlist = func1(member, memberlist);
+            break;
+        case '2':
+            func2(memberlist);
+            break;
+        case '3':
+            break;
+        case '4':
+            break;
+        case '5':
+            func5(memberlist);
+            break;
+        case '6':
+            break;
+        case '7':
+            break;
+        case 'r':
+            remove("member.xls");
+            remove("member.txt");
+            printf("已清除所有数据！\n");
+            exit(0);
+            break;
+        case '8':
+            break;
+        case 'd':
+            memberlist=dele(memberlist);
+            break;
+        case 'i':
+            memberlist=insert(memberlist);
+            break;
+        default:
+            printf("Error：无法识别输入信息，请重新操作！\n");
+        }
+    } while (i != '0');
+    //退出程序时，释放链表的内存
+    member=memberlist.head;
+    free(member);
+    return 0;
+}
